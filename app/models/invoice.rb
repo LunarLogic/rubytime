a8a876e0fe62c3f5f09d387f1b19fe61560417f9
Marcin Kulik <marcin.kulik@llp.pl>
2008-10-06 16:13:02 +0200

    models improvements

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,10 +1,18 @@
 class Invoice
   include DataMapper::Resource
 
-  property :id,            Serial
-
-  property :client_id,    Integer, :nullable => false
+  property :id,          Serial
+  property :name,        String, :nullable => false, :unique => true
+  property :notes,       Text
+  property :client_id,   Integer, :nullable => false
+  # property :user_id,   Integer, :nullable => false # what is this user for?
+  property :issued_at,   DateTime, :default => nil
+  property :created_at,  DateTime
  
   belongs_to :client
+  # belongs_to :user # what is this user for?
   
+  def issued?
+    !self.issued_at.nil?
+  end
 end
