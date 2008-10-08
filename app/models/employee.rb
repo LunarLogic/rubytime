7d34315c245bc78bbf0ee485922f2f0c39a9ff7b
Marcin Kulik <marcin.kulik@llp.pl>
2008-10-08 12:02:19 +0200

    employee changes

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,3 @@
 class Employee < User
+  property :role, Enum[:developer, :tester, :project_manager], :default => :developer
 end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -2,6 +2,20 @@ require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
 describe Employee do
 
-  it "should have specs"
+  it "should be created" do
+    employee = Employee.make
+    lambda { employee.save }.should change(Employee, :count).by(1)
+    employee.role.should == :developer
+  end
 
+  it "should be created as tester" do
+    employee = Employee.gen(:role => :tester)
+    employee.role.should == :tester
+  end
+
+  it "should not be created if has invalid role" do
+    employee = Employee.make(:role => :invalid)
+    employee.save.should be_false
+    employee.errors.on(:role).size.should == 1
+  end
 end
