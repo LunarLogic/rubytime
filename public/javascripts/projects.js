f3920998f06dc897b3cfbcb5a283056d053d4cec
lotus <maciej.lotkowski@gmail.com>
2008-10-14 15:57:03 +0200

    refactored js

--- public/javascripts/projects.js
+++ public/javascripts/projects.js
@@ -1,23 +1,7 @@
-var Projects = {
-  init: function() {
-    Projects.initRemoveProject();
-  },
-  
-  initRemoveProject: function() {
-    $("#projects a.delete").click(function () {
-      if (confirm('Are you sure to remove this project?')) {
-        $(this).parent().parent().addClass('to_delete');
-        $.delete_($(this).attr('href'), {}, Projects._onRemoved);
-      }
-      return false;
-    });
-  },
-
-  _onRemoved: function() {
-    $("tr.to_delete").fadeOut();
-  }
-}
-
 $(function() {
-  Projects.init();
+  $("#projects a.delete").click(function (e) {
+    if (confirm('Are you sure to remove this project?'))
+      $.delete_($(this).attr('href'), {}, function() { $(e.target).parents('tr').remove(); });
+    return false;
+  });
 });
