c3d85a29c95a9e6827a49bee3922406094fad008
lotus <maciej.lotkowski@gmail.com>
2008-10-16 10:11:00 +0200

    removed empty file

--- public/javascripts/projects.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  $("#projects a.delete").click(function (e) {
-    if (confirm('Are you sure to remove this project?'))
-      $.delete_($(this).attr('href'), {}, function() { $(e.target).parents('tr').remove(); });
-    return false;
-  });
-});
