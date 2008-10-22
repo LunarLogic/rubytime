2d17e621da430284a495eb54438dc305f7f457c6
Piotr Solnica <piotr@zenbe.com>
2008-10-22 10:16:08 +0200

    merb deps upgrade 0.9.9 => 0.9.10

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,6 +1,6 @@
-dependency "merb-assets", "0.9.9"        # Provides link_to, asset_path, auto_link, image_tag methods (and lots more)
-dependency "merb-helpers", "0.9.9"       # Provides the form, date/time, and other helpers
-dependency "merb-mailer", "0.9.9"        # Integrates mail support via Merb Mailer
+dependency "merb-assets", "0.9.10"        # Provides link_to, asset_path, auto_link, image_tag methods (and lots more)
+dependency "merb-helpers", "0.9.10"       # Provides the form, date/time, and other helpers
+dependency "merb-mailer", "0.9.10"        # Integrates mail support via Merb Mailer
 dependency "merb_has_flash"
 
 dependency "dm-core", "0.9.7"         # The datamapper ORM
@@ -11,10 +11,10 @@ dependency "dm-validations", "0.9.7"  # Validation framework
 dependency "dm-sweatshop"
 dependency "dm-observer", "0.9.7"
 
-# dependency "merb-action-args", "0.9.9"   # Provides support for querystring arguments to be passed in to controller actions
-# dependency "merb-cache", "0.9.9"         # Provides your application with caching functions 
-# dependency "merb-slices", "0.9.9"        # Provides a mechanism for letting plugins provide controllers, views, etc. to your app
-# dependency "merb-auth", "0.9.9"          # An authentication slice (Merb's equivalent to Rails' restful authentication)
+# dependency "merb-action-args", "0.9.10"   # Provides support for querystring arguments to be passed in to controller actions
+# dependency "merb-cache", "0.9.10"         # Provides your application with caching functions 
+# dependency "merb-slices", "0.9.10"        # Provides a mechanism for letting plugins provide controllers, views, etc. to your app
+# dependency "merb-auth", "0.9.10"          # An authentication slice (Merb's equivalent to Rails' restful authentication)
 
 #dependency "dm-migrations" #, "0.9.7"   # Make incremental changes to your database.
 dependency "randexp", ">=0.1.3"
