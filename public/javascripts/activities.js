e498267bb81d49f1c5ec250a2d6037a4d4acd91b
Marcin Kulik <marcin.kulik@llp.pl>
2008-10-29 15:23:11 +0100

    added zebra to tables, styled filter and add activity form

--- public/javascripts/activities.js
+++ public/javascripts/activities.js
@@ -47,6 +47,7 @@ var Activities = {
       // load with GET request
       $("#primary").load(form.url()+'?' + form.serialize(), null, function() {
         //form.find("input[type=submit]").removeAttr("disabled");
+        $(this).zebra();
       });
       return false;
     });
