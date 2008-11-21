e6be931fcab936449809a647cb7f418c850b5feb
Marcin Kulik <marcin.kulik@llp.pl>
2008-11-21 14:55:58 +0100

    added remove and edit icons to tables, changed show-link into name-link

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module Merb
   module UsersHelper
     def link_to_calendar(user)
-      link_to "Calendar", url(:user_calendar, user) if user.is_employee?
+      link_to image_tag("icons/calendar.png", :title => "Calendar"), url(:user_calendar, user) if user.is_employee?
     end
   end
 end # Merb
