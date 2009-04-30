40307a538c819a9451584ab80cf37d6db6dd5a3a
Rafal G <myco@myco.(none)>
2009-04-30 15:00:35 +0200

    removing vacation for users - connected with #86

--- app/controllers/free_days.rb
+++ app/controllers/free_days.rb
@@ -7,7 +7,7 @@ class FreeDays < Application
     if current_user.id.to_s == user_id or current_user.is_admin?
       @free_day = FreeDay.new(:user_id => user_id, :date => date)
       if @free_day.save
-        render_success "You have taken vacation at " + date
+        render_success "You have just taken vacation at " + date
       else
         render_failure "Couldn't take vacation"
       end
@@ -18,10 +18,10 @@ class FreeDays < Application
   def delete
     date = params[:date]
     user_id = params[:user_id]
-    if current_user.id == user_id or current_user.is_admin?
+    if current_user.id.to_s == user_id or current_user.is_admin?
       @free_day = FreeDay.all(:user_id => user_id, :date => date)
       if @free_day.destroy!
-        render_success "You have remove vacation at " + date
+        render_success "Vacation at " + date + " was removed"
       else
         render_failure "Couldn't remove vacation"
       end
