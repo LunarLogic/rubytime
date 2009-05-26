92f06d55402e3259af4755fde2ca56aa89f7df48
Piotr Solnica <piotr@zenbe.com>
2009-05-26 17:48:50 +0200

    removed duplicated has many projects assoc from employee model (since it is defined in user)

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,7 +2,6 @@ class Employee < User
   validates_present :role
   
   has n, :activities
-  has n, :projects, :through => :activities
   
   before :destroy do
     throw :halt if activities.count > 0
