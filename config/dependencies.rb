ef95b285ddfed0e1c612ae964d59b6bbf79d7499
Piotr Solnica <piotr@zenbe.com>
2009-05-26 17:48:01 +0200

    updated dependencies

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,6 +1,7 @@
-merb_gems_version = "~>1.0"
-dm_gems_version   = "~>0.9.8"
+merb_gems_version = "~>1.0.11"
+dm_gems_version   = "~>0.9.11"
 
+dependency "merb_datamapper", merb_gems_version
 dependency "merb-assets", merb_gems_version        # Provides link_to, asset_path, auto_link, image_tag methods (and lots more)
 dependency "merb-helpers", merb_gems_version       # Provides the form, date/time, and other helpers
 dependency "merb-mailer", merb_gems_version        # Integrates mail support via Merb Mailer
@@ -12,17 +13,17 @@ dependency "merb-auth-slice-password", merb_gems_version
 dependency "dm-core", dm_gems_version         # The datamapper ORM
 dependency "dm-aggregates", dm_gems_version   # Provides your DM models with count, sum, avg, min, max, etc.
 dependency "dm-timestamps", dm_gems_version   # Automatically populate created_at, created_on, etc. when those properties are present.
-dependency "dm-types" , dm_gems_version        # Provides additional types, including csv, json, yaml.
+dependency "dm-types" , dm_gems_version       # Provides additional types, including csv, json, yaml.
 dependency "dm-validations", dm_gems_version  # Validation framework
-dependency "dm-constraints", '0.9.9'  # Validation framework
+dependency "dm-migrations", dm_gems_version   # Migrations support
 dependency "dm-observer", dm_gems_version
-dependency "dm-sweatshop", dm_gems_version
-dependency "dm-serializer", '0.9.9'  # dm_gems_version
+dependency "dm-sweatshop", dm_gems_version    # Data generator
+dependency "dm-serializer", dm_gems_version   # Serialization
+dependency "dm-constraints", '0.9.9'          # Validation framework
 
 # dependency "merb-action-args", "0.9.10"   # Provides support for querystring arguments to be passed in to controller actions
 # dependency "merb-cache", "0.9.10"         # Provides your application with caching functions 
 
-#dependency "dm-migrations" #, "0.9.7"   # Make incremental changes to your database.
 dependency "randexp", ">=0.1.3"
 dependency "chronic", ">=0.2.3"
 dependency "html5", ">=0.10.0"
