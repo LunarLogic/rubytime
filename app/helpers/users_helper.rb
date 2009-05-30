7c1d57e64f6d17c843f8c49b0be96f791c6da2b1
Piotr Solnica <piotr@zenbe.com>
2009-05-30 12:59:53 +0200

    cleaned up settings form and added checked attribute to radio buttons

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,5 +3,18 @@ module Merb
     def link_to_calendar(user)
       link_to image_tag("icons/calendar.png", :title => "Calendar"), url(:user_calendar, user) if user.is_employee?
     end
+
+    def recent_days_on_list_desc(v)
+      "#{v} days"
+  end
+
+    def date_format_desc(v)
+      "#{v.capitalize} (#{Rubytime::DATE_FORMATS[v.to_sym][:description]})"
+    end
+
+    def radio_options(user, property, symbols)
+      radio_group(property, symbols.map { |s|
+        { :value => s.to_s, :label => yield(s.to_s), :checked => user.attribute_get(property) == s }})
+    end
   end
 end # Merb
