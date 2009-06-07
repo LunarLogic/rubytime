56aa0a5939588a41c30533735833afac8d383015
Piotr Solnica <piotr@zenbe.com>
2009-06-07 17:52:45 +0200

    fixed activities_table helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -81,16 +81,17 @@ module Merb
     end
     
     def activities_table(activities, options={})
-      default_options = { :show_checkboxes => false, :show_users => true, :show_details_link => true, :show_edit_link => true,
-                          :show_delete_link => true, :show_exclude_from_invoice_link => false, :expanded => false,
-                          :show_date => true }
+      default_options = { :show_checkboxes => false, :show_users => true,
+                          :show_details_link => true, :show_edit_link => true,
+                          :show_delete_link => true, :show_exclude_from_invoice_link => false,
+                          :expanded => false, :show_date => true }
+
       options = default_options.merge(options)
 
-      table_opts = {
-        :class => 'activities list wide',
-        :id => "#{options[:table_id]}"}.reject!{|k,v| v.blank?}
+      table_opts = { :class => 'activities list wide', :id => "#{options[:table_id]}"}
+      table_opts.reject!{|k,v| v.blank?}
 
-      tag(:table,table_opts) do
+      tag(:table, table_opts) do
         html =  %(<tr>)
         html << %(<th class="checkbox">#{check_box :class => "activity_select_all"}</th>) if options[:show_checkboxes]
         html << %(<th>#{image_tag("icons/project.png", :alt => 'project') if options[:show_header_icons]} Project</th>) if options[:show_project]
