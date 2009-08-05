b0887d9bb225d776f636157cc817675277b791b6
Jacek Mikrut <jacek.mikrut@llp.pl>
2009-08-05 15:41:17 +0200

    Refactored code.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -14,15 +14,19 @@ class Employee < User
     m.dispatch_and_deliver(:timesheet_nagger, :to => email, :from => Rubytime::CONFIG[:mail_from], :subject => "RubyTime timesheet nagger!")
   end
   
+  def self.without_activities_on(date)
+    all.reject { |employee| employee.has_activities_on?(date) }
+  end
+  
   def self.send_timesheet_naggers_for(date, logger = Logger.new(nil))
-    all.reject { |employee| employee.has_activities_on?(date) }.each do |employee|
+    Employee.without_activities_on(date).each do |employee|
       logger.info "Sending timesheet nagger email to #{employee.name}."
       employee.send_timesheet_nagger_for(date)
     end
   end
   
   def self.send_timesheet_reporter_for(date, email, logger = Logger.new(nil))
-    m = UserMailer.new(:employees_without_activities => all.reject { |employee| employee.has_activities_on?(date) }, :day_without_activities => date)
+    m = UserMailer.new(:employees_without_activities => Employee.without_activities_on(date), :day_without_activities => date)
     m.dispatch_and_deliver(:timesheet_reporter, :to => email, :from => Rubytime::CONFIG[:mail_from], :subject => "RubyTime timesheet report")
   end
 end
