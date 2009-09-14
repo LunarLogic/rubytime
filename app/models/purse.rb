d66da5de3e78a76bbeca13ff8dc7369758e77b4f
Jacek Mikrut <jacek.mikrut@llp.pl>
2009-09-14 11:21:44 +0200

    Created Purse class.

---
+++ app/models/purse.rb
@@ -0,0 +1,22 @@
+class Purse
+  def initialize
+    @money_by_currency = {}
+  end
+  
+  def <<(money)
+    @money_by_currency[money.currency] ||= Money.new(0, money.currency)
+    @money_by_currency[money.currency] += money
+  end
+  
+  def currencies
+    @money_by_currency.keys.sort_by { |currency| currency.singular_name }
+  end
+  
+  def [](currency)
+    @money_by_currency[currency] || Money.new(0, currency)
+  end
+  
+  def to_s
+    currencies.map { |currency| self[currency].to_s }.join(' and ')
+  end
+end
---
+++ spec/models/purse_spec.rb
@@ -0,0 +1,42 @@
+require File.join( File.dirname(__FILE__), '..', "spec_helper" )
+
+describe Purse do
+  
+  before { @purse = Purse.new }
+  
+  context "after initialize" do
+    it "should be empty" do
+      @purse.currencies.should == []
+    end
+  end
+  
+  it "should allow to put money inside and show content" do
+    @purse.currencies.should == []
+    @purse[fx(:euro)  ].should == Money.new(0.00, fx(:euro))
+    @purse[fx(:dollar)].should == Money.new(0.00, fx(:dollar))
+    
+    
+    @purse << Money.new(11.25, fx(:euro))
+    
+    @purse.currencies.should == [ fx(:euro) ]
+    @purse[fx(:euro)  ].should == Money.new(11.25, fx(:euro))
+    @purse[fx(:dollar)].should == Money.new( 0.00, fx(:dollar))
+    
+    
+    @purse << Money.new(2.50, fx(:euro))
+    @purse << Money.new(9.98, fx(:dollar))
+    
+    @purse.currencies.should == [ fx(:dollar), fx(:euro) ]
+    @purse[fx(:euro)  ].should == Money.new(13.75, fx(:euro))
+    @purse[fx(:dollar)].should == Money.new( 9.98, fx(:dollar))
+  end
+  
+  describe "#to_s" do
+    it "should return content as string" do
+      @purse << Money.new(2.50, fx(:euro))
+      @purse << Money.new(9.98, fx(:dollar))
+      
+      @purse.to_s.should == Money.new(9.98, fx(:dollar)).to_s + ' and ' + Money.new(2.50, fx(:euro)).to_s
+    end
+  end
+end
