05acaa86a94a89c38824535bbc87444dd1ac7fa8
Jacek Mikrut <jacek.mikrut@llp.pl>
2009-09-25 14:52:02 +0200

    Added :free_days_access_key setting.

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,15 +1,32 @@
 class Setting
   include DataMapper::Resource
   
+  ACCESS_KEY_CHARS = "1234567890qwrtpsdfghjklzxcvbnmQWRTPSDFGHJKLZXCVBNM"
+  
   property :id, Serial
   property :enable_notifications, Boolean, :default  => false, :nullable => false
-  
+  property :free_days_access_key, String,  :default => Proc.new { Setting.generate_free_days_access_key }, :nullable => false
+    
   def self.get
     first || create
   end
   
   def self.enable_notifications
     get.enable_notifications
+  end
+  
+  def generate_free_days_access_key
+    self.free_days_access_key = Setting.generate_free_days_access_key
+  end
+  
+  def self.free_days_access_key
+    get.free_days_access_key
+  end
+  
+  private
+
+  def self.generate_free_days_access_key
+    ACCESS_KEY_CHARS.generate_random(50)
   end
 
 end
--- spec/models/setting_spec.rb
+++ spec/models/setting_spec.rb
@@ -24,5 +24,39 @@ describe Setting do
       end
     end
   end
+  
+  describe '.free_days_access_key method' do
+    
+    context 'when a Setting record exists' do
+      before { @setting = Setting.create! }
+      it 'should return the value of the setting' do
+        Setting.first.update_attributes :free_days_access_key => 'thekey'
+        Setting.free_days_access_key.should == 'thekey'
+      end
+    end
+    
+    context 'when no Setting record exists' do
+      before { Setting.stub!(:generate_free_days_access_key => 'generated_key') }
+      it 'should create a Setting record' do
+        expect{Setting.free_days_access_key}.to change{Setting.count}.by(1)
+      end
+      
+      it 'should return the value of the setting' do
+        Setting.free_days_access_key.should == 'generated_key'
+      end
+    end
+  end
+  
+  describe '#generate_free_days_access_key method' do
+    before do
+      Setting.stub!(:generate_free_days_access_key => 'generated_key')
+      @setting = Setting.create!(:free_days_access_key => 'one')
+    end
+    
+    it 'should generate new value of :free_days_access_key' do
+      @setting.generate_free_days_access_key
+      @setting.free_days_access_key.should == 'generated_key'
+    end
+  end
 
 end
