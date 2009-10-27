f0762d23907b616270c3646c4abb2d433b17b041
Jakub Suder <jakub.suder@gmail.com>
2009-10-27 16:26:51 +0100

    added /users/with_activities action which shows only selected users

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -12,6 +12,18 @@ class Users < Application
     display @users
   end
 
+  def with_activities
+    only_provides :json
+    @users = if current_user.is_admin?
+               Employee.all(:order => [:name]).with_activities
+             elsif current_user.is_client_user?
+               Employee.all(:order => [:name]).with_activities_for_client(current_user.client)
+             else
+               raise Forbidden
+             end
+    display @users
+  end
+
   def show
     display @user
   end
