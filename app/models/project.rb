3c7be45c7b4cb9aa384c31712a26d62b1552fbca
Jakub Suder <jakub.suder@gmail.com>
2010-01-22 11:46:57 +0100

    add has_activities flag to projects in project index JSON action

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,7 +7,8 @@ class Project
   property :client_id,    Integer, :nullable => false, :index => true
   property :active,       Boolean, :nullable => false, :default => true
   property :created_at,   DateTime
-  
+
+  attr_accessor :has_activities
   belongs_to :client
   has n, :activities
   has n, :users, :through => :activities
@@ -19,7 +20,21 @@ class Project
   def self.active
     all(:active => true)
   end
-  
+
+  def self.visible_for(user)
+    if user.is_admin?
+      all
+    elsif user.is_employee?
+      active
+    else  # client
+      user.client.projects
+    end
+  end
+
+  def self.with_activities_for(user)
+    all('activities.user_id' => user.id, :unique => true)
+  end
+
   def calendar_viewable?(user)
     user.client == self.client || user.is_admin?
   end
