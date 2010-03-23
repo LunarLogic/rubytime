e985f6cbbedaf4ce39047efd3110aeb16462d60d
Jacek Mikrut <jacek.mikrut@llp.pl>
2010-03-23 15:31:29 +0100

    Improved the code ;-)

--- app/controllers/activities.rb
+++ app/controllers/activities.rb
@@ -195,11 +195,11 @@ protected
   def convert_to_csv(activities)
     report = StringIO.new
     CSV::Writer.generate(report, ',') do |csv|
-      csv << %w(Client Project Role User Date Hours) + ActivityCustomProperty.all.each.map { |p| p.name_with_unit } + %w(Type SubType Comments)
+      csv << %w(Client Project Role User Date Hours) + ActivityCustomProperty.all.map { |p| p.name_with_unit } + %w(Type SubType Comments)
       activities.each do |activity|
         csv << [activity.project.client.name, activity.project.name, activity.user.role.name, activity.user.name, 
                 activity.date, format("%.2f", activity.minutes / 60.0)] +
-                ActivityCustomProperty.all.each.map { |p| activity.custom_properties[p.id] } +
+                ActivityCustomProperty.all.map { |p| activity.custom_properties[p.id] } +
                [(activity.main_activity_type ? activity.main_activity_type.name : nil),
                 (activity.sub_activity_type ? activity.sub_activity_type.name : nil),
                 activity.comments.strip]
