4178d0d28f0ae570cfb9273b7e9635b6435fa36b
Ania <anna.lesniak@llp.pl>
2010-11-29 09:35:50 +0100

    Inactive users should not be displayed in select field on admin calendar view

--- app/controllers/activities.rb
+++ app/controllers/activities.rb
@@ -113,7 +113,7 @@ class Activities < Application
   def calendar
     if current_user.is_admin?
       if params[:user_id]
-        @users = Employee.all(:order => [:name.asc])
+        @users = Employee.all(:active => true, :order => [:name.asc])
       else
         @projects = Project.all(:order => [:name.asc])
       end
