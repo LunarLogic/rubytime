3c46e3719a42f4e6b4301aa715aee933891af2b5
Ania <anna.lesniak@llp.pl>
2010-12-14 12:02:25 +0100

    Inactive clients should be included in select on edit project page

--- app/controllers/projects.rb
+++ app/controllers/projects.rb
@@ -5,7 +5,7 @@ class Projects < Application
   before :ensure_can_list_projects, :only => [:index]
   before :load_project, :only => [:edit, :update, :destroy, :show]
   before :load_projects, :only => [:index, :create]
-  before :load_clients, :only => [:index, :new, :create, :edit, :update]
+  before :load_clients, :only => [:index, :new, :create, :update]
   
   def index
     @project = Project.new :client => Client.get(params[:client_id])
@@ -36,6 +36,7 @@ class Projects < Application
   end
 
   def edit
+    @clients = Client.all
     render
   end
   
