777b7f88e8168a5f0077b97c182228b08fc4a252
obrok <pawel.obrok@llp.pl>
2011-09-19 18:03:30 +0200

    Migration to Rails 3: More fixes to activity forms

--- spec/views/activities_spec.rb
+++ spec/views/activities_spec.rb
@@ -4,7 +4,8 @@ describe "activities/index.html.erb" do
   before(:each) do
     login(:employee)
     @search_criteria = SearchCriteria.new({:date_from => DateTime.now}, @current_user)
-    @activities = @search_criteria.found_activities
+    @activities = [Activity.generate]
+    @custom_properties = [ActivityCustomProperty.generate]
   end
 
   it "should render successfully" do
