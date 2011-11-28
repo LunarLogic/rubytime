ac5d8b67647e0763c323e4dafe28fb90616d8234
Jakub Suder <jakub.suder@gmail.com>
2011-11-28 11:45:46 +0100

    fixed RSpec deprecation messages

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -1,23 +1,23 @@
-Spec::Matchers.define :have_errors_on do |attr|
+RSpec::Matchers.define :have_errors_on do |attr|
   match do |resource|
     resource.valid?
     not resource.errors.on(attr).nil?
   end
 end
 
-Spec::Matchers.define :be_forbidden do
+RSpec::Matchers.define :be_forbidden do
   match do |response|
     response.status == 403
   end
 end
 
-Spec::Matchers.define :be_not_found do
+RSpec::Matchers.define :be_not_found do
   match do |response|
     response.status == 404
   end
 end
 
-Spec::Matchers.define :be_not_acceptable do
+RSpec::Matchers.define :be_not_acceptable do
   match do |response|
     response.status == 406
   end
