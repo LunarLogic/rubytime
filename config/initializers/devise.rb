6a2a39fb7fb56c840ee43f94307412b78386290f
Jakub Suder <jakub.suder@gmail.com>
2011-12-05 17:23:16 +0100

    send emails with proper From: field

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "rubytime@example.com"
+  config.mailer_sender = Rubytime::CONFIG[:mail_from]
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
