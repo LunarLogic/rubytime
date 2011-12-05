dfe850ca32ce98115d10577fd4851f0ca484cf95
Jakub Suder <jakub.suder@gmail.com>
2011-12-05 18:01:20 +0100

    removed most of merb_helper

--- app/helpers/merb_helper.rb
+++ app/helpers/merb_helper.rb
@@ -11,20 +11,4 @@ module MerbHelper
     render :partial => partial.to_s, :locals => locals
   end
 
-  def submit(name, options)
-    submit_tag(name, options)
-  end
-
-  def throw_content(name, content)
-    content_for(name, content)
-  end
-
-  def label(name, text, content_or_options = nil, options = nil)
-    if text.is_a?(Hash)
-      # This is a Merb-style call
-      super(text[:for], name)      
-    else
-      super(name, text, content_or_options, options)
-    end
-  end
 end
